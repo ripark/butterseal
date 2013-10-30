779336c75d46aeafa5a4641c08d0795648cebe48
dogmelons <kyle.easterling@gmail.com>
2013-10-30 03:29:09 -0400

    Added Pause Screen, new Puzzle and Title Screen

--- src/ButterSeal-android/src/edu/smcm/ButterSealGame/Seal.java
+++ src/ButterSeal-android/src/edu/smcm/ButterSealGame/Seal.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.tiled.TileAtlas;
 import com.badlogic.gdx.graphics.g2d.tiled.TileMapRenderer;
 import com.badlogic.gdx.graphics.g2d.tiled.TiledLoader;
@@ -16,7 +17,6 @@ import com.badlogic.gdx.graphics.g2d.tiled.TiledObjectGroup;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.TimeUtils;
  
 public class Seal implements ApplicationListener
 {
@@ -37,24 +37,35 @@ public class Seal implements ApplicationListener
 	
 	private Texture dpadImage;
 	private Texture aButtonImage;
+	private Texture menuButtonImage;
+	private Texture resumeButtonImage;
+	private Texture titleScreenImage;
+	private Texture darknessImage;
 	private Rectangle dpad;
 	private Rectangle aButton;
+	private Rectangle menuButton;
+	private Rectangle resumeButton;
+	private Rectangle titleScreen;
+	private Rectangle[][] darkness;
+	
 	private Music BestMusic;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
+	private SpriteBatch darknessBatch;
 	private Vector3 touchPos;
 	private TileMapRenderer tileMapRenderer;
 	private Player player;
 	private Matrix4 matrix;
 	private TiledMap tiledMap;
 	
-	private long lastActionTime;
-	
 	private boolean isMoving;
-	private boolean isPressed;
+	private boolean isPaused;
+	private boolean AIsPressed;
 	private int moveAmount;
 	
-	private enum MapQuadrant
+    TextureRegion currentPlayerFrame;
+	
+	enum MapQuadrant
 	{
 		NORTH,
 		EAST,
@@ -68,15 +79,10 @@ public class Seal implements ApplicationListener
 	private static class Tile
 	{
 		public int tileId;
-		
-		public enum Kind
-		{
-			WALL,
-			playTheBestMusic
-		};
-		public Kind kind;
 	}
-	private Tile currentTile;
+	private Tile currentTile0;
+	//private Tile currentTile1;
+	
 	
 	
 	/**
@@ -96,10 +102,16 @@ public class Seal implements ApplicationListener
 		
 		dpadImage = new Texture(Gdx.files.internal("dpad1.png"));
 		aButtonImage = new Texture(Gdx.files.internal("aButton.png"));
+		menuButtonImage = new Texture(Gdx.files.internal("menuButton.png"));
+		resumeButtonImage = new Texture(Gdx.files.internal("resumeButton2.png"));
+		darknessImage = new Texture(Gdx.files.internal("darkness.png"));
+		titleScreenImage = new Texture(Gdx.files.internal("titleScreen.png"));
+
 		uiHeight = dpadImage.getHeight();
 		uiWidth = dpadImage.getWidth();
 		uiHeight3 = uiHeight/3;
 		uiWidth3 = uiWidth/3;
+		
 		BestMusic = Gdx.audio.newMusic(Gdx.files.internal("Best.mp3"));
 		
 		dpad = new Rectangle();
@@ -112,17 +124,34 @@ public class Seal implements ApplicationListener
 		aButton.x = width-uiWidth;
 		aButton.y = 0;
 		
+		menuButton = new Rectangle();
+		menuButton.x = width-menuButtonImage.getWidth();
+		menuButton.y = height-menuButtonImage.getHeight();
+		
+		resumeButton = new Rectangle();
+		resumeButton.x = (width/2) - (resumeButtonImage.getWidth()/2);
+		resumeButton.y = (height/2) - (resumeButtonImage.getHeight()/2);
+		
+		titleScreen = new Rectangle();
+		titleScreen.x = 0;
+		titleScreen.y = 0;
+		titleScreen.width = width;
+		titleScreen.height = height;
+		
 		batch = new SpriteBatch();
+		darknessBatch = new SpriteBatch();
 		
 		touchPos = new Vector3();
 		
 		tiledMap = new TiledMap();
-		tiledMap = TiledLoader.createMap(Gdx.files.internal("collisionmap/collisionTestDesert.tmx"));
-		TileAtlas tileAtlas = new TileAtlas(tiledMap, Gdx.files.internal("collisionmap"));
+		tiledMap = TiledLoader.createMap(Gdx.files.internal("ice-cave/ice-cave.tmx"));
+		TileAtlas tileAtlas = new TileAtlas(tiledMap, Gdx.files.internal("ice-cave"));
 		tileMapRenderer = new TileMapRenderer(tiledMap, tileAtlas, 12, 12);
 		
 		mapWidth = tiledMap.width * tiledMap.tileWidth;
 		mapHeight = tiledMap.height * tiledMap.tileHeight;
+		
+		darkness = new Rectangle[tiledMap.height][tiledMap.width];
 		
 		BestMusic.setLooping(true);
 		
@@ -138,24 +167,95 @@ public class Seal implements ApplicationListener
 	            	player = new Player(object.x + 16, mapHeight - (object.y+16));
 	            	camera.position.set(object.x + 16, mapHeight - (object.y+16), 0);
 	        	 }
+	        	 if("darkness".equals(object.type)){
+	        		 darkness[object.y/32][object.x/32] = new Rectangle();
+	        		 darkness[object.y/32][object.x/32].y = Math.abs(object.y-tiledMap.tileHeight*tiledMap.height+32);
+	        		 darkness[object.y/32][object.x/32].x = object.x;
+	        	 }
 	         }
 		}
 				
-		currentTileX = 26;
-		currentTileY = 45;
-		currentTile = new Tile();
-		currentTile.tileId = tiledMap.layers.get(0).tiles[currentTileY][currentTileX];
+		currentTileX = 20;
+		currentTileY = 22;
+		currentTile0 = new Tile();
+		currentTile0.tileId = tiledMap.layers.get(0).tiles[currentTileY][currentTileX];
 		
 		quadrant = MapQuadrant.INVALID;
 		isMoving = false;
-		isPressed = false;
+		isPaused = false;
+		AIsPressed = false;
 		moveAmount = 0;
+		
 	}
  
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	boolean isTitle = true;
+	/**
+	 * basic render function. Calls for input first, then clears the screen. Then, it updates the camera, renders the map
+	 * to the camera, draws the player to scale of the camera, and draws the d-pad and 'A' button.
+	 */
+	@Override
+	public void render()
+	{
+		while(isTitle)
+		{
+			Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT );
+			batch.begin();
+			batch.draw(titleScreenImage, titleScreen.x, titleScreen.y);
+			batch.end();
+			if (Gdx.input.isTouched())
+				isTitle=false;
+			return;
+		}
+		if(isPaused)
+		{
+			pause();
+			return;
+		}
+		
+		if(!isMoving)
+		{
+			System.out.println(player.toString());
+			player.SetCurrentFrame();
+		}
+		else
+		{
+			player.SetCurrentFrame(quadrant);
+		}
+		
+		movePlayerCamera();
+		
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT );
+		
+		camera.update();
+		tileMapRenderer.render(camera);
+		
+		player.Draw(matrix);
+		
+		darknessBatch.setProjectionMatrix(matrix);
+		darknessBatch.begin();
+		for(int i = 0; i < tiledMap.height; i++)
+		{
+			for(int j = 0; j < tiledMap.width; j++)
+			{
+				if(darkness[i][j] != null)
+				{
+					darknessBatch.draw(darknessImage, darkness[i][j].x, darkness[i][j].y);
+				}
+			}
+		}
+		darknessBatch.end();
+		
+		batch.begin();
+		batch.draw(dpadImage, dpad.x, dpad.y);
+		batch.draw(aButtonImage, aButton.x, aButton.y);
+		batch.draw(menuButtonImage, menuButton.x, menuButton.y);
+		batch.end();
 	}
  
 	/**
@@ -196,41 +296,76 @@ public class Seal implements ApplicationListener
 		else
 			isMoving = false;			
 	}
- 
+	
 	/**
-	 * basic render function. Calls for input first, then clears the screen. Then, it updates the camera, renders the map
-	 * to the camera, draws the player to scale of the camera, and draws the d-pad and 'A' button.
+	 * checks to see if the player is touching inside the d-pad, then checks to see which quadrant of the d-pad is being touched.
+	 * if input is valid, calls Interaction() for certain properties. If property checks go through, acts on those properties
+	 * by calling related functions.
 	 */
-	@Override
-	public void render() 
-	{		
- 
-		movePlayerCamera();
-		
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT );
-		
-		camera.update();
-		tileMapRenderer.render(camera);
-		
-		player.Draw(matrix);
-		
-		batch.begin();
-		batch.draw(dpadImage, dpad.x, dpad.y);
-		batch.draw(aButtonImage, aButton.x, aButton.y);
-		batch.end();
+	private void handleInput()
+	{
+		if(Gdx.input.isTouched()) 
+		{
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			
+			if(isInAbutton())
+			{
+				if(BeaconInteraction("Beacon")) {
+					ActivateBeacon();
+				}
+				AIsPressed = true;
+				return;
+			}
+			
+			if(isInMenuButton())
+			{
+				isPaused = true;
+				return;
+			}
+			
+			if(isInDpad())
+			{
+				findQuadrant();
+				switch(quadrant)
+				{
+					case WEST:				
+						if(!Interaction("wall")) {
+							moveAmount = 32;
+							isMoving = true;
+							currentTileX--;
+						}
+						break;
+					case EAST:				
+						if(!Interaction("wall")) {
+							moveAmount = 32;
+							isMoving = true;
+							currentTileX++;
+					
+						}
+						break;
+					case NORTH:				
+						if(!Interaction("wall")) {
+							moveAmount = 32;
+							isMoving = true;
+							currentTileY--;
+						}
+						break;
+					case SOUTH:
+						if(!Interaction("wall")) {
+							moveAmount = 32;
+							isMoving = true;
+							currentTileY++;
+						}
+						break;
+					default:
+						break;
+				}			
+			}	
+		}
+		AIsPressed = false;
 	}
 	
 	// Helper functions for handleInput
-	private boolean isInDpad()
-	{
-		if((touchPos.y < (height - uiHeight)))
-			return false;
-		if(touchPos.x > uiWidth)
-			return false;
-		
-		return true;		
-	}
-	
 	private boolean isInAbutton()
 	{
 		if((touchPos.y < (height - uiHeight3)) && (touchPos.y > (height - (uiHeight3*2))) && (touchPos.x < (width - uiWidth3)) &&
@@ -241,38 +376,31 @@ public class Seal implements ApplicationListener
 		return false;
 	}
 	
-	/**
-	 * checks to see the property of the tile the player wants to move onto by getting the tile ID and
-	 * checking the property value. Checks property value against Property, if check goes through, returns true.
-	 * @param Property the property you are checking for to check for interaction.
-	 * @return
-	 */
-	private boolean Interaction(String Property)
+	private boolean isInMenuButton()
 	{
-		switch(quadrant)
+		if((touchPos.y < (height - menuButtonImage.getHeight())) && (touchPos.x > (width - menuButtonImage.getWidth())))
 		{
-		case WEST:
-			currentTile.tileId = tiledMap.layers.get(0).tiles[currentTileY][currentTileX-1];
-			break;
-		case EAST:
-			currentTile.tileId = tiledMap.layers.get(0).tiles[currentTileY][currentTileX+1];
-			break;
-		case NORTH:
-			currentTile.tileId = tiledMap.layers.get(0).tiles[currentTileY-1][currentTileX];
-			break;
-		case SOUTH:
-			currentTile.tileId = tiledMap.layers.get(0).tiles[currentTileY+1][currentTileX];
-			break;
-		default:
-			break;
+			return true;
 		}
-		if (tiledMap.getTileProperty(currentTile.tileId, Property) != null) {
-			if(Property == "playTheBestMusic") {
-				return true;
-			}
-			else if(Property == "wall")	{
-				return true;
-			}
+		return false;
+	}
+	
+	private boolean isInDpad()
+	{
+		if((touchPos.y < (height - uiHeight)))
+			return false;
+		if(touchPos.x > uiWidth)
+			return false;
+		
+		return true;		
+	}
+	
+	private boolean isInResumeButton()
+	{
+		if((touchPos.x < ((width/2) + resumeButtonImage.getWidth()/2)) && (touchPos.x > ((width/2) - resumeButtonImage.getWidth()/2))
+			&& (touchPos.y < ((height/2) + resumeButtonImage.getHeight()/2)) && (touchPos.y > ((height/2) - resumeButtonImage.getHeight()/2)))
+		{
+			return true;
 		}
 		return false;
 	}
@@ -296,76 +424,94 @@ public class Seal implements ApplicationListener
 	}
 	
 	/**
-	 * checks to see if the player is touching inside the d-pad, then checks to see which quadrant of the d-pad is being touched.
-	 * if input is valid, calls Interaction() for certain properties. If property checks go through, acts on those properties
-	 * by calling related functions.
+	 * checks to see the property of the tile the player wants to move onto by getting the tile ID and
+	 * checking the property value. Checks property value against Property, if check goes through, returns true.
+	 * @param Property the property you are checking for to check for interaction.
+	 * @return
 	 */
-	private void handleInput()
+	private boolean Interaction(String Property)
 	{
-		if(Gdx.input.isTouched()) 
+		switch(quadrant)
 		{
-			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			
-			if(isInAbutton())
+		case WEST:
+			currentTile0.tileId = tiledMap.layers.get(0).tiles[currentTileY][currentTileX-1];
+			if(darkness[currentTileY][currentTileX-1] != null)
 			{
-				if(Interaction("playTheBestMusic")) {
-					checkToggleMusic();
-				}
-				isPressed = true;
-				return;
+				if(Property.equals("wall"))
+						return true;
 			}
-			
-			if(!isInDpad())
+			break;
+		case EAST:
+			currentTile0.tileId = tiledMap.layers.get(0).tiles[currentTileY][currentTileX+1];
+			if(darkness[currentTileY][currentTileX+1] != null)
 			{
-				return;
+				if(Property.equals("wall"))
+						return true;
 			}
-			
-			findQuadrant();
-			
-			switch(quadrant)
+			break;
+		case NORTH:
+			currentTile0.tileId = tiledMap.layers.get(0).tiles[currentTileY-1][currentTileX];
+			if(darkness[currentTileY-1][currentTileX] != null)
 			{
-			case WEST:				
-				if(!Interaction("wall")) {
-					moveAmount = 32;
-					isMoving = true;
-					currentTileX--;
-				}
-				break;
-			case EAST:				
-				if(!Interaction("wall")) {
-					moveAmount = 32;
-					isMoving = true;
-					currentTileX++;
-					
-				}
-				break;
-			case NORTH:				
-				if(!Interaction("wall")) {
-					moveAmount = 32;
-					isMoving = true;
-					currentTileY--;
-				}
-				break;
-			case SOUTH:
-				if(!Interaction("wall")) {
-					moveAmount = 32;
-					isMoving = true;
-					currentTileY++;
-				}
-				break;
-			default:
-				break;
-			}			
+				if(Property.equals("wall"))
+						return true;
+			}
+			break;
+		case SOUTH:
+			currentTile0.tileId = tiledMap.layers.get(0).tiles[currentTileY+1][currentTileX];
+			if(darkness[currentTileY+1][currentTileX] != null)
+			{
+				if(Property.equals("wall"))
+						return true;
+			}
+			break;
+		default:
+			break;
 		}
-		isPressed = false;
+		if ((tiledMap.getTileProperty(currentTile0.tileId, Property) != null)) 
+		{
+			if(Property == "playTheBestMusic") 
+			{
+				return true;
+			}
+			else if(Property == "wall")	
+			{
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
+	public boolean BeaconInteraction(String Property)
+	{
+		if(tiledMap.getTileProperty(currentTile0.tileId, Property) != null)
+		{
+			if(Property.equals("Beacon"))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void ActivateBeacon()
+	{
+		for(int i = 0; i < darkness.length ; i ++) {
+			darkness[i][currentTileX] = null;
+		}
+		for(int i = 0; i < darkness[0].length; i++) {
+			darkness[currentTileY][i] = null;
+		}
+		//tiledMap.layers.get(0).tiles[currentTileX][currentTileY] = 0;
+	}
+
 	/**
 	 * toggles music. Only applies the first frame the button is pressed.
 	 */
 	public void checkToggleMusic() 
 	{
-		if(!isPressed) {
+		if(!AIsPressed) {
 			if(BestMusic.isPlaying()) {
 					BestMusic.pause();	 
 			}
@@ -377,14 +523,42 @@ public class Seal implements ApplicationListener
  
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
+		touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+		if(isInResumeButton())
+		{
+			resume();
+			return;
+		}
 		
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT );
+		
+		camera.update();
+		tileMapRenderer.render(camera);
+		
+		player.Draw(matrix);
+		
+		darknessBatch.setProjectionMatrix(matrix);
+		darknessBatch.begin();
+		for(int i = 0; i < tiledMap.height; i++)
+		{
+			for(int j = 0; j < tiledMap.width; j++)
+			{
+				if(darkness[i][j] != null)
+				{
+					darknessBatch.draw(darknessImage, darkness[i][j].x, darkness[i][j].y);
+				}
+			}
+		}
+		darknessBatch.end();
+		
+		batch.begin();
+		batch.draw(resumeButtonImage, resumeButton.x, resumeButton.y);
+		batch.end();
 	}
  
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
+		isPaused = false;		
 	}
  
 	@Override
