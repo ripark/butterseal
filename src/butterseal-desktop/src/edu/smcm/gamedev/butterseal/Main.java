30b80d14b090c27847b622c086650f7dd5661428
Sean Allred <allred.sean@gmail.com>
2013-11-03 16:17:19 -0500

    tabula rasa --- part duex
    Re-generated project from LibGDX JAR.

---
+++ src/butterseal-desktop/src/edu/smcm/gamedev/butterseal/Main.java
@@ -0,0 +1,16 @@
+package edu.smcm.gamedev.butterseal;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "butterseal";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new ButterSeal(), cfg);
+	}
+}
