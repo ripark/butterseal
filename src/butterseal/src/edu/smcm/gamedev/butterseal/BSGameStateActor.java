c3dad2607199526eb3a82fbf3dca995126d007a4
Sean Allred <allred.sean@gmail.com>
2013-11-13 23:41:51 -0500

    Add actor interface

---
+++ src/butterseal/src/edu/smcm/gamedev/butterseal/BSGameStateActor.java
@@ -0,0 +1,9 @@
+package edu.smcm.gamedev.butterseal;
+
+public interface BSGameStateActor {
+	/**
+	 * Perform the appropriate actions given the game state.
+	 * @param player
+	 */
+	public void act(BSPlayer player);
+}
