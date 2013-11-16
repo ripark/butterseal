1023dcb0d1b139860c01b4b21892824463a74692
Sean Allred <allred.sean@gmail.com>
2013-11-16 12:08:13 -0500

    BSMap improvements
    - change the way maps are defined
    - change the way maps are loaded
    - change the way properties are returned

--- src/butterseal/src/edu/smcm/gamedev/butterseal/BSMap.java
+++ src/butterseal/src/edu/smcm/gamedev/butterseal/BSMap.java
@@ -15,29 +15,43 @@ public enum BSMap {
                 // game logic
             }
 	}),
-    ICE_CAVE(BSAssets.ICE_CAVE.getAssetPath(), "ice-cave", new Runnable() {
+    ICE_CAVE(BSAssets.ICE_CAVE, "ice-cave", new Runnable() {
             public void run() {
                 // game logic
             }
 	});
 	
     BSGameState state;
+    TiledMap map;
+    TileAtlas atlas;
+    TileMapRenderer renderer;
 
-    BSMap(String asset, String key, Runnable action) {
-        // TODO Auto-generated constructor stub
-    }
-	
-    void draw(SpriteBatch batch) {
-		
+    BSMap(BSAssets asset, String key, Runnable action) {
+        this.map = TiledLoader.createMap(Gdx.files.internal(asset.getAssetPath()));
+        // TODO: compare with L148 of @9a50ff1735; possibly incorrect syntax
+        this.atlas = new TileAtlas(this.map, Gdx.files.internal(asset.getAssetPath()));
+        this.renderer = new TileMapRenderer(this.map, this.atlas, 12, 12);
     }
 
-    public Map<String, String> getTileProperties(BSTile tile) {
-        return null;
+    void draw(OrthographicCamera camera) {
+        this.renderer.render(camera);
+    }
+
+    public List<Map<String, String>> getTileProperties(BSPlayer player) {
+        // TODO: I think the following constructor exists.
+        // Double-check.  I wand to create the array list with the
+        // same size as how many layers there are in the map.
+        List<Map<String, String>> ret = new ArrayList<Map<String, String>>(this.map.layers.size());
+        for(TiledMapTileLayer layer : this.map.layers) {
+            Cell cell = layer.getCell(player.currentTile.x, player.currentTile.y);
+            TiledMapTile tile = cell.getTile();
+            // TODO: this function returns `MapProperties`, but what exactly is that?
+            ret.add(tile.getProperties();
+        }
     }
 
     public void draw() {
         // TODO Auto-generated method stub
-		
     }
 }
 
