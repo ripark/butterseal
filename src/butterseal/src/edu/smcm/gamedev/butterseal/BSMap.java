eb42002afd0a6b638d3426f0ce7aa084a86d82b8
Sean Allred <allred.sean@gmail.com>
2013-11-21 14:02:10 -0500

    prune dead code

--- src/butterseal/src/edu/smcm/gamedev/butterseal/BSMap.java
+++ src/butterseal/src/edu/smcm/gamedev/butterseal/BSMap.java
@@ -1,17 +1,7 @@
 package edu.smcm.gamedev.butterseal;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTile;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
@@ -51,28 +41,9 @@ public enum BSMap {
         this.action = action;
     }
 
-
     void draw(OrthographicCamera camera) {
         this.renderer.setView(camera);
         this.renderer.render();
-    }
-
-    public List<Map<String, String>> getTileProperties(BSPlayer player) {
-        List<Map<String, String>> ret = new ArrayList<Map<String, String>>(this.map.getLayers().getCount());
-        for(MapLayer layer : this.map.getLayers()) {
-            // TODO: cast may cause error
-            Cell cell = ((TiledMapTileLayer) layer).getCell(player.currentTile.x, player.currentTile.y);
-            TiledMapTile tile = cell.getTile();
-            Iterator<String> keys = tile.getProperties().getKeys();
-            Map<String, String> this_map = new HashMap<String, String>();
-            while(keys.hasNext()) {
-                String key = keys.next();
-                String val = (String) tile.getProperties().get(key);
-                this_map.put(key, val);
-            }
-            ret.add(this_map);
-        }
-        return ret;
     }
 }
 
