40bc21cc71f9a9406d906b69a65afb375217fd39
Sean Allred <allred.sean@gmail.com>
2013-12-08 00:37:57 -0500

    Add standard tileset

---
+++ src/butterseal-android/assets/data/tilesets/ice-cave.tsx
@@ -0,0 +1,204 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ice-cave" tilewidth="64" tileheight="64">
+ <image source="ice-cave.png" width="1024" height="1024"/>
+ <terraintypes>
+ </terraintypes>
+ <tile id="9">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="light" value="torch"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="beacon" value="off"/>
+   <property name="light" value="beacon"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="beacon" value="on"/>
+   <property name="light" value="beacon"/>
+  </properties>
+ </tile>
+ <tile id="22" terrain="0,0,0,0"/>
+ <tile id="40">
+  <properties>
+   <property name="player" value="from"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="88">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="89">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="player" value="to"/>
+  </properties>
+ </tile>
+ <tile id="166">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="176">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="177">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="178">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="182">
+  <properties>
+   <property name="wall" value="true"/>
+  </properties>
+ </tile>
+ <tile id="255">
+  <properties>
+   <property name="invisible" value="true"/>
+  </properties>
+ </tile>
+</tileset>
